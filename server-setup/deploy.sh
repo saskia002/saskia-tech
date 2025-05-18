e08cb6447f09a2397d3fb9430a4be21a44a18606
Saskia Kriibi <90192374+saskia002@users.noreply.github.com>
2025-05-18 10:25:15 +0300

    Create deploy.sh

---
+++ server-setup/deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# chmod +x deploy.sh
+
+# some shell commnad
+# if [ $? -eq 0 ] <-- if 0 then success else failed
+
+set -e
+set -o pipefail
+
+# Ensure the script is run as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Please run this script as root or using sudo."
+    exit 1
+fi
+
+
+if git pull; then
+	echo "Git pull completed successfully."
+else
+	echo "Git pull failed."
+	exit 1
+fi
+
+
+if npm run build; then
+	echo "Build completed successfully."
+else
+	echo "Build failed."
+	service web restart
+	exit 1
+fi
+
+
+if service web restart; then
+	echo "Service restarted successfully."
+else
+	echo "Failed to restart the service."
+	exit 1
+fi
