9ac147c525f832f650274778d1908330559b1eb1
Saskia <90192374+saskia002@users.noreply.github.com>
2025-12-18 17:26:08 +0200

    Update page.tsx

--- src/app/minecraft/page.tsx
+++ src/app/minecraft/page.tsx
@@ -3,10 +3,11 @@ export default function Page() {
 		<main className="w-full h-max inline-flex flex-col items-center mb-8">
 			<section className="min-md:w-4/6 max-w-[1000px] max-md:w-8/10">
 				<h2>Minecraft server</h2>
-				<p>
+				<p>Someone hacked my server, so no more fun ;(</p>
+				{/*<p>
 					You can join my MC server using this link: <u>saskia.tech</u>
 				</p>
-				<p>Also, pls dont hax my linus sever! 😎</p>
+				<p>Also, pls dont hax my linus sever! 😎</p>*/}
 			</section>
 		</main>
 	);
